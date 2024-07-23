fe203debf2d99f11edf0f179eb23416741cb9545
Chris Yanez <cy2019@hotmail.com>
2024-07-23 10:02:41 -0400

    backup commit

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const path = require('path');
 const view_routes = require('./routes/view_routes');
 const api_routes = require('./routes/api_routes');
 const app = express();
+
+
+
+
+
+
+//just node server.js
+app.listen(6358, () => {//gets server started
+    console.log('Server started');
+});
