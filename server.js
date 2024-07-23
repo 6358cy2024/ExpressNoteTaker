e7dc6a0ad24dd23a5ce4defa0ac6dcc42d4f9c52
Chris Yanez <cy2019@hotmail.com>
2024-07-23 09:34:58 -0400

    first steps

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const path = require('path');
+const view_routes = require('./routes/view_routes');
+const api_routes = require('./routes/api_routes');
+const app = express();
